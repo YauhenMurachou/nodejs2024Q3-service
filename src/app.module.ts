c921049d96b5beb1e2056b7be5dcda47871d2c40
YauhenMurachou <emurach1992@gmail.com>
2024-11-12 02:46:32 +0300

    feat:favs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumController } from './album/album.controller';
 import { ArtistController } from './artist/artist.controller';
+import { FavsController } from './favs/favs.controller';
 import { TrackController } from './track/track.controller';
 import { UserController } from './user/user.controller';
 
 import { AlbumService } from './album/album/album.service';
 import { ArtistService } from './artist/artist/artist.service';
+import { FavsService } from './favs/favs/favs.service';
 import { TrackService } from './track/track/track.service';
 import { UserService } from './user/user/user.service';
 
@@ -17,6 +19,7 @@ import { UserService } from './user/user/user.service';
     AppController,
     AlbumController,
     ArtistController,
+    FavsController,
     TrackController,
     UserController,
   ],
@@ -24,6 +27,7 @@ import { UserService } from './user/user/user.service';
     AppService,
     AlbumService,
     ArtistService,
+    FavsService,
     TrackService,
     UserService,
   ],
